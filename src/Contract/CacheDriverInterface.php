fe2bd6cb79e614094c428db291d4421a0e8a4539
ZhuChongLin <8210856@qq.com>
2024-04-08 23:34:00 +0800

    缓存驱动器约束

---
+++ src/Contract/CacheDriverInterface.php
@@ -0,0 +1,229 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | ViSwoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace ViSwoole\Cache\Contract;
+
+use Closure;
+use DateTime;
+
+interface CacheDriverInterface
+{
+  /**
+   * 自增缓存（针对数值缓存）
+   *
+   * @access public
+   * @param string $key 缓存标识
+   * @param int $step 步长
+   * @return false|int
+   */
+  public function inc(string $key, int $step = 1): bool|int;
+
+  /**
+   * 自减缓存（针对数值缓存）
+   *
+   * @access public
+   * @param string $key 缓存标识
+   * @param int $step 步长
+   * @return false|int
+   */
+  public function dec(string $key, int $step = 1): bool|int;
+
+  /**
+   * 写入缓存
+   *
+   * @access public
+   * @param string $key 缓存标识
+   * @param mixed $value 存储数据
+   * @param DateTime|int $expire 有效时间（秒）
+   * @param bool $NX 如果为true则缓存不存在才会写入
+   * @return bool
+   */
+  public function set(string $key, mixed $value, DateTime|int $expire = 0, bool $NX = false): bool;
+
+  /**
+   * 追加（数组）缓存
+   * @access public
+   * @param string $key 缓存标识
+   * @param mixed $value 存储数据
+   * @return void
+   */
+  public function push(string $key, mixed $value): void;
+
+  /**
+   * 读取缓存并删除
+   * @access public
+   * @param string $key 缓存标识
+   * @return mixed
+   */
+  public function pull(string $key): mixed;
+
+  /**
+   * 判断缓存
+   *
+   * @access public
+   * @param string $key 缓存标识
+   * @return bool
+   */
+  public function has(string $key): bool;
+
+  /**
+   * 读取缓存
+   *
+   * @access public
+   * @param string $key 不带前缀的名称
+   * @param mixed $default 默认值
+   * @return mixed
+   */
+  public function get(string $key, mixed $default = null): mixed;
+
+  /**
+   * 清除缓存
+   *
+   * @access public
+   * @return bool
+   */
+  public function clear(): bool;
+
+  /**
+   * 删除缓存
+   *
+   * @access public
+   * @param array|string $keys
+   * @return false|int
+   */
+  public function delete(array|string $keys): false|int;
+
+  /**
+   * 取锁/上锁
+   *
+   * @access public
+   * @param string $scene 业务场景
+   * @param int $expire 锁过期时间/秒
+   * @param bool $autoUnlock 在程序运行完毕后自动解锁默认false
+   * @param int $retry 等待尝试次数
+   * @param int|float $sleep 等待休眠时间/秒 最小精度为毫秒（0.001 秒）
+   * @return int 成功返回锁id 失败抛出系统繁忙错误
+   */
+  public function lock(
+    string    $scene,
+    int       $expire = 10,
+    bool      $autoUnlock = false,
+    int       $retry = 5,
+    int|float $sleep = 0.2
+  ): int;
+
+  /**
+   * 解除锁
+   *
+   * @access public
+   * @param int|null $id 锁id
+   * @return bool
+   */
+  public function unlock(int $id = null): bool;
+
+  /**
+   * 关闭连接(实例销毁会自动关闭连接/归还连接到连接池)
+   *
+   * @return void
+   */
+  public function close(): void;
+
+  /**
+   * 标签
+   *
+   * @access public
+   * @param string|array $tag
+   * @return CacheTagInterface
+   */
+  public function tag(string|array $tag): CacheTagInterface;
+
+  /**
+   * 获取所有缓存标签
+   *
+   * @access public
+   * @return array|false
+   */
+  public function getTags(): array|false;
+
+  /**
+   * 获取实际标签名
+   *
+   * @access public
+   * @param string $tag 标签名
+   * @return string
+   */
+  public function getTagKey(string $tag): string;
+
+  /**
+   * 获取连接实例(例如redis驱动获取redis实例实现更多的操作)
+   *
+   * @return mixed
+   */
+  public function handler(): mixed;
+
+  /**
+   * 获取实际的缓存标识
+   *
+   * @access public
+   * @param string $key 缓存标识
+   * @return string
+   */
+  public function getCacheKey(string $key): string;
+
+  /**
+   * 往数组集合中添加记录
+   *
+   * @access public
+   * @param string $key 缓存名称
+   * @param array|string $array 集合
+   * @param bool $serialize 是否序列化
+   * @return false|int 如果写入的值已存在则会返回false，其他返回写入的数量
+   */
+  public function sAddArray(string $key, array|string $array, bool $serialize = true): false|int;
+
+  /**
+   * 获取数组集合
+   *
+   * @access public
+   * @param string $key 集合名称
+   * @param bool $serialize 是否序列化
+   * @return array|false
+   */
+  public function getArray(string $key, bool $serialize = true): array|false;
+
+  /**
+   * 从集合中移除元素
+   *
+   * @access public
+   * @param string $key 集合名称
+   * @param array|string $values 要删除的值
+   * @param bool $serialize 是否序列化
+   * @return false|int
+   */
+  public function sRemoveArray(string $key, array|string $values, bool $serialize = true
+  ): false|int;
+
+  /**
+   * 设置序列化方法
+   *
+   * @access public
+   * @param string|Closure $set
+   * @param string|Closure $get
+   * @return static
+   */
+  public function setSerialize(
+    string|Closure $set = 'serialize', string|Closure $get = 'unserialize'
+  ): static;
+}
---
+++ src/Contract/CacheTagInterface.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | ViSwoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace ViSwoole\Cache\Contract;
+
+use DateTime;
+
+interface CacheTagInterface
+{
+  public function __construct(string|array $tags, CacheDriverInterface $driver);
+
+  /**
+   * 写入缓存
+   *
+   * @access public
+   * @param string $key 缓存名称
+   * @param mixed $value 存储数据
+   * @param DateTime|int $expire 有效时间（秒）
+   * @return bool
+   */
+  public function set(string $key, mixed $value, DateTime|int $expire = -1): bool;
+
+  /**
+   * 清除标签缓存
+   *
+   * @access public
+   * @return void
+   */
+  public function clear(): void;
+
+  /**
+   * 删除对应标签下的
+   *
+   * @param string|array $keys 要删除的缓存
+   * @return void
+   */
+  public function remove(string|array $keys): void;
+
+  /**
+   * 追加缓存标识到标签
+   *
+   * @access public
+   * @param string $key
+   * @return bool
+   */
+  public function push(string $key): bool;
+}
