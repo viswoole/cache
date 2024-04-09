1786e52e27acd0af1c90386fea73dbf13a7ea0b0
ZhuChongLin <8210856@qq.com>
2024-04-09 22:55:57 +0800

    Redis连接池

---
+++ src/Pool/RedisPool.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | ViSwoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace ViSwoole\Cache\Pool;
+
+use Override;
+use Redis;
+use RedisException;
+use ViSwoole\Cache\RedisConfig;
+use ViSwoole\Core\ConnectionPool;
+
+/**
+ * Redis连接池
+ */
+class RedisPool extends ConnectionPool
+{
+  public function __construct(protected RedisConfig $redisConfig)
+  {
+    parent::__construct($redisConfig->pool_max_size, $redisConfig->pool_fill_size);
+  }
+
+  /**
+   * 必须实现创建连接方法
+   *
+   * @return Redis 返回一个可用的连接对象
+   * @throws RedisException
+   */
+  #[Override] protected function createConnection(): Redis
+  {
+    $redis = new Redis();
+    $redis->connect(
+      $this->redisConfig->host,
+      $this->redisConfig->port,
+      $this->redisConfig->timeout,
+      null,
+      $this->redisConfig->retry_interval,
+      $this->redisConfig->read_timeout
+    );
+    if (!empty($this->redisConfig->password)) {
+      $result = $redis->auth($this->redisConfig->password);
+      if (true !== $result) throw new RedisException('Redis auth fail');
+    }
+    if ($this->redisConfig->db_index !== 0) {
+      $result = $redis->select($this->redisConfig->db_index);
+      if (true !== $result) throw new RedisException('Redis select db fail');
+    }
+    return $redis;
+  }
+
+  /**
+   * 可实现此方法在获取或归还连接时检测连接是否可用
+   *
+   * @param mixed $connection
+   * @return bool 如果返回true则代表连接可用
+   */
+  #[Override] protected function connectionDetection(mixed $connection): bool
+  {
+    if (!($connection instanceof Redis)) return false;
+    $result = $connection->ping();
+    return $result === true;
+  }
+}
